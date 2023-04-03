dc8b60097edd0626c407d66c938766497ea2e03b
Santtu Nykänen <santtu.nykanen@gofore.com>
2023-04-03 18:29:34 +0200

    1st working proto
    * Queries data from Azure Postgres
    * Uploads data as JSON to Azure Blob Storage

--- src/functions/bukettiTimerTrigger.ts
+++ src/functions/bukettiTimerTrigger.ts
@@ -1,68 +1,84 @@
 import { app, InvocationContext, Timer } from "@azure/functions";
-import pg from 'pg';
+import { BlobServiceClient } from "@azure/storage-blob";
+import { v4 } from 'uuid';
+const pg = require('pg');
 
-export async function bukettiTimerTrigger(myTimer: Timer, context: InvocationContext): Promise<void> {
+require('dotenv').config()
 
-    const config = {
-        host: 'buketti-azure-function-test-db.postgres.database.azure.com',
-        user: 'testuser',
-        password: 'buketti#2',
-        database: 'postgres',
-        port: 5432,
-        ssl: true
-    };
+const blobServiceClient = BlobServiceClient.fromConnectionString(process.env.BLOB_CONNECTION_STRING);
+const containerClient = blobServiceClient.getContainerClient(process.env.BLOB_CONTAINER_NAME);
 
-    const client = new pg.Client(config);
+export const bukettiTimerTrigger = async (myTimer: Timer, context: InvocationContext): Promise<void> => {
+  const config = {
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    port: 5432,
+    ssl: true
+  };
 
-    client.connect(err => {
-        if (err) throw err;
-        else {
-            // createDatabase();
-            queryDatabase();
-        }
+  const client = new pg.Client(config);
+
+  client.connect(async err => {
+    if (err) throw err;
+    else {
+      // createDatabase(client);
+      const rows = await queryDatabase(client);
+      await uploadDataToAzure(rows);
+    }
+  });
+
+  context.log('Timer function processed request.');
+};
+
+const uploadDataToAzure = async (data) => {
+  const filename = `uploaded-data-${v4()}`;
+  const blockBlobClient = containerClient.getBlockBlobClient(filename);
+  const response = await blockBlobClient.upload(JSON.stringify(data), data.length);
+  console.log(`Uploaded data ${JSON.stringify(data)} as ${filename}`);
+  if (response._response.status !== 201) {
+    throw new Error(
+      `Error uploading document ${blockBlobClient.name} to container ${blockBlobClient.containerName}`
+    );
+  }
+};
+
+// const createDatabase = (client) => {
+//   const query = `
+//     DROP TABLE IF EXISTS inventory;
+//     CREATE TABLE inventory (id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER);
+//     INSERT INTO inventory (name, quantity) VALUES ('banana', 150);
+//     INSERT INTO inventory (name, quantity) VALUES ('orange', 154);
+//     INSERT INTO inventory (name, quantity) VALUES ('apple', 100);
+//   `;
+//
+//   client
+//     .query(query)
+//     .then(() => {
+//       console.log('Table created successfully!');
+//       client.end();
+//     })
+//     .catch(err => console.log(err))
+//     .then(() => {
+//       console.log('Finished execution, exiting now');
+//       process.exit();
+//     });
+// };
+
+const queryDatabase = async (client) => {
+  console.log(`Running query to PostgreSQL server`);
+  const query = 'SELECT * FROM inventory;';
+  return await client.query(query)
+    .then(res => {
+      return res.rows;
+    })
+    .catch(err => {
+      console.log(err);
     });
-
-    function createDatabase() {
-        const query = `
-            DROP TABLE IF EXISTS inventory;
-            CREATE TABLE inventory (id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER);
-            INSERT INTO inventory (name, quantity) VALUES ('banana', 150);
-            INSERT INTO inventory (name, quantity) VALUES ('orange', 154);
-            INSERT INTO inventory (name, quantity) VALUES ('apple', 100);
-        `;
-
-        client
-            .query(query)
-            .then(() => {
-                console.log('Table created successfully!');
-                client.end(console.log('Closed client connection'));
-            })
-            .catch(err => console.log(err))
-            .then(() => {
-                console.log('Finished execution, exiting now');
-                process.exit();
-            });
-    }
-
-    function queryDatabase() {
-        console.log(`Running query to PostgreSQL server: ${config.host}`);
-        const query = 'SELECT * FROM inventory;';
-        client.query(query)
-            .then(res => {
-                const rows = res.rows;
-                rows.map(row => {
-                    console.log(`Read: ${JSON.stringify(row)}`);
-                });
-                process.exit();
-            })
-            .catch(err => {
-                console.log(err);
-            });
-    }
-    context.log('Timer function processed request.');
-}
+};
 
 app.timer('bukettiTimerTrigger', {
-    schedule: '*/20 * * * * *',
-    handler: bukettiTimerTrigger
+  schedule: '* * * * *',
+  handler: bukettiTimerTrigger
 });
